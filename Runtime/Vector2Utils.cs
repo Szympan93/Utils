f0a46ff6937593add95ec98f33cc3287adc948be
Szymon Migowski <szymon.m93@gmail.com>
2026-04-03 07:37:50 +0200

    Add converters V2->V3

--- Runtime/Vector2Utils.cs
+++ Runtime/Vector2Utils.cs
@@ -17,5 +17,41 @@ namespace FunFact.Utils
         public static Vector2 ToY0(this Vector2 v) => new(v.y, 0);
         public static Vector2 ToYX(this Vector2 v) => new(v.y, v.x);
         public static Vector2 ToYY(this Vector2 v) => new(v.y, v.y);
+        
+        public static Vector3 To000(this Vector2 v) => new(0  , 0  , 0  );
+        public static Vector3 To00X(this Vector2 v) => new(0  , 0  , v.x);
+        public static Vector3 To00Y(this Vector2 v) => new(0  , 0  , v.y);
+        
+        public static Vector3 To0X0(this Vector2 v) => new(0  , v.x, 0  );
+        public static Vector3 To0XX(this Vector2 v) => new(0  , v.x, v.x);
+        public static Vector3 To0XY(this Vector2 v) => new(0  , v.x, v.y);
+        
+        public static Vector3 To0Y0(this Vector2 v) => new(0  , v.y, 0  );
+        public static Vector3 To0YX(this Vector2 v) => new(0  , v.y, v.x);
+        public static Vector3 To0YY(this Vector2 v) => new(0  , v.y, v.y);
+        
+        public static Vector3 ToX00(this Vector2 v) => new(v.x, 0  , 0  );
+        public static Vector3 ToX0X(this Vector2 v) => new(v.x, 0  , v.x);
+        public static Vector3 ToX0Y(this Vector2 v) => new(v.x, 0  , v.y);
+        
+        public static Vector3 ToXX0(this Vector2 v) => new(v.x, v.x, 0  );
+        public static Vector3 ToXXX(this Vector2 v) => new(v.x, v.x, v.x);
+        public static Vector3 ToXXY(this Vector2 v) => new(v.x, v.x, v.y);
+        
+        public static Vector3 ToXY0(this Vector2 v) => new(v.x, v.y, 0  );
+        public static Vector3 ToXYX(this Vector2 v) => new(v.x, v.y, v.x);
+        public static Vector3 ToXYY(this Vector2 v) => new(v.x, v.y, v.y);
+        
+        public static Vector3 ToY00(this Vector2 v) => new(v.y, 0  , 0  );
+        public static Vector3 ToY0X(this Vector2 v) => new(v.y, 0  , v.x);
+        public static Vector3 ToY0Y(this Vector2 v) => new(v.y, 0  , v.y);
+        
+        public static Vector3 ToYX0(this Vector2 v) => new(v.y, v.x, 0  );
+        public static Vector3 ToYXX(this Vector2 v) => new(v.y, v.x, v.x);
+        public static Vector3 ToYXY(this Vector2 v) => new(v.y, v.x, v.y);
+        
+        public static Vector3 ToYY0(this Vector2 v) => new(v.y, v.y, 0  );
+        public static Vector3 ToYYX(this Vector2 v) => new(v.y, v.y, v.x);
+        public static Vector3 ToYYY(this Vector2 v) => new(v.y, v.y, v.y);
     }
 }
